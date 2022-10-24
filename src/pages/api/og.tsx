aa3888ebd00f1d423b05645c5e966298bdbfe266
neb <nebb_bb@protonmail.com>
2022-10-24 00:26:55 -0500

    adjust height

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -48,6 +48,7 @@ export default async function handler() {
               color: "#fd691f",
               fontWeight: "bold",
               display: "flex",
+              marginBottom: "48px",
             }}
           >
             {toWords(Math.round(usd.initialAmount / 1000000) * 1000000)} dollars
