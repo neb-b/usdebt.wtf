72acac1a8fc93d7858aba080d96f111018e0e23f
neb <nebb_bb@protonmail.com>
2022-10-24 13:38:45 -0500

    ?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,17 +9,19 @@ function App({ Component, pageProps }: AppProps) {
   const [isMounted, setIsMounted] = React.useState(false)
   React.useEffect(() => setIsMounted(true), [])
 
+  const ogImage = `https://usdebt.wtf/api/og?reset=${Date.now()}`
+
   return (
     <>
       <Head>
         <title>usdebt.wtf</title>
         <meta name="og:title" content="usdebt.wtf" />
-        <meta name="og:image" content="https://usdebt.wtf/api/og" />
+        <meta name="og:image" content={ogImage} />
         <link rel="icon" type="image/x-icon" href="https://usdebt.wtf/favicon.png"></link>
         <meta property="og:description" content="wtf is going on with the us debt?" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://usdebt.wtf/api/og" />
+        <meta name="twitter:image" content={ogImage} />
         <meta name="twitter:title" content="usdebt.wtf" />
       </Head>
       <ChakraProvider theme={theme}>{isMounted && <Component {...pageProps} />}</ChakraProvider>
